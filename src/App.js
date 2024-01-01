6cf8c95f3ab810deb68ff36e13cf5b03cd828534
wanchanok062 <anvacom2@gmail.com>
2024-01-02 00:10:05 +0700

    add new pages for learning

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-
+import Navbar from "./Navbar";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Create from "./Create";
+import BlogDetails from "./BlogDetails";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/blog/:id" element={<BlogDetails />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
