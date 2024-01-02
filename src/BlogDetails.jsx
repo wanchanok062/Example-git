0596ad0de5ccc8695bcb05f4adaacf95978498bc
wanchanok062 <anvacom2@gmail.com>
2024-01-02 14:54:37 +0700

    All done

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,14 +1,33 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 
 const BlogDetails = () => {
     const { id } = useParams();
-
-    return ( 
+    const { data: blog, isPadding, errorMassage: error } = useFetch('http://localhost:8002/blogs/'+id)
+    const navigate = useNavigate();
+    
+    const handleDelete = () => {
+        fetch('http://localhost:8002/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            navigate('/')
+        })
+    }
+    return (
         <div className="blog-details">
-            <h2>Blog detail - {id}</h2>
+            {isPadding && <div>Loading....</div>}
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
+            <button onClick={handleDelete}>Delete</button>
         </div>
-     );
+    );
 }
 
 export default BlogDetails;
